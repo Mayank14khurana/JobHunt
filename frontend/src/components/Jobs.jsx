f554d803df2437a918bc32d63b51345f48dbab7c
Mayank14khurana <153353155+Mayank14khurana@users.noreply.github.com>
2024-11-08 21:57:05 +0530

    Update Jobs.jsx

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavbarTemp from './NavbarTemp'
 import FilterCards from './FilterCards'
 import Job from './Job';
 import { useSelector } from 'react-redux';
 
-const jobsArray =[1,2,3,4,5,6,7,8];
 const Jobs = () => {
-  const {allJobs} =useSelector(store=>store.job);
+  const {allJobs ,searchedQuery} =useSelector(store=>store.job);
+  const [filterJobs,setFilterJobs]=useState(allJobs);
+   useEffect(()=>{
+    if(searchedQuery){
+
+      const filteredJobs= allJobs.filter((job)=>{
+        return      job?.title?.toLowerCase().includes(searchedQuery.toLowerCase())||
+             job?.description?.toLowerCase().includes(searchedQuery.toLowerCase())||
+              job?.location?.toLowerCase().includes(searchedQuery.toLowerCase()) 
+      }
+      )
+
+      setFilterJobs(filteredJobs);
+    }
+    if(!searchedQuery) {
+     setFilterJobs(allJobs);
+    }
+   },[searchedQuery])
   return (
     <div className='text-start'>
       <NavbarTemp/>
@@ -17,11 +33,11 @@ const Jobs = () => {
             </div>
    
       {
-        allJobs.length<=0 ? <span>Job not found</span>:(
+        filterJobs.length<=0 ? <span>Job not found</span>:(
             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'> 
              <div className='grid grid-cols-3 gap-4'>
            {
-             allJobs.map((job)=>(<div key={job?._id}> <Job job={job} /> </div>) )
+             filterJobs.map((job)=>(<div key={job?._id}> <Job job={job} /> </div>) )
            }
              </div>
             </div>
@@ -37,3 +53,4 @@ const Jobs = () => {
 }
 
 export default Jobs
+
